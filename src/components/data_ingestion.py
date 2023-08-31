fe3003180fb7d918d10d9ae0ea24d76a0c19e34f
DESKTOP-9DEJDE4\shayn <masudeecs@gmail.com>
2023-08-31 07:12:21 +0600

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,60 @@
+# -> Read the data from data source, split the data for train and test part
+# -> after that data transformation will happen.
+
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    """
+        whenever we're performing data ingestion component there should be some inputs that maybe required by data ingestion
+        The input will be where we should save train data, test data and raw data. Those kind of input will be created another
+        kind of class. Dataclass decorator use for writing class variable directly
+    """
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()  # all DataIngestionConfig clas variable are now inside ingestion_config
+
+    def initiate_data_ingestion(self):
+        """read the data from database."""
+        logging.info("Entered the data ingestion method or component.")
+
+        try:
+            df = pd.read_csv(r'C:\Users\shayn\PycharmProjects\mlproject\notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated.")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of the data is completed.")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
